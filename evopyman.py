a45bdc760981b7c46877240f3e608caa48731f57
roolin <roolin@gmail.com>
2009-12-14 00:35:29 +0100

    jeszce raz

--- evopyman.py
+++ evopyman.py
@@ -2,70 +2,21 @@ from random import Random, randint, random
 from pygene.gene import BaseGene, FloatGene
 from pygene.organism import Organism
 from pygene.population import Population
+from hashlib import sha1
+from instruction import *
     
-organismLen = 5
+organismLen = 10
 
-MyRandom = Random()
-
-Direction = {
-    "north" : 0,
-    "n" : 0,
-    0 : "north",
-    "south" : 1,
-    "s" : 1,
-    1 : "south",
-    "east"  : 2,
-    "e" : 2,
-    2 : "east",
-    "west"  : 3,
-    "w" : 3,
-    3 : "west"
-    }
-    
-Element = {
-    "Ghost" : 0,
-    0 : "Ghost",
-    "SmallDot" : 1,
-    1 : "SmallDot",
-    "BigDot" : 2,
-    2 : "BigDot"
-    }
-
-Mode = { "Good" : 0,
-    0 : "Good",
-    "Bad" : 1,
-    1 : "Bad"
-    }
-
-class Instruction:
-    def __init__(self):
-        self.randomValue()
-    def randomValue(self):
-        self.direction = Direction[randint(0,3)]
-        self.mode = Mode[randint(0,1)]
-        self.element = Element[randint(0,2)]
-        self.distStart = randint(1, 10)
-    def __repr__(self):
-        return "%s : %s - %s : %s - %s -" % (
-            self.direction, Direction[self.direction], 
-            self.mode, Mode[self.mode],
-            self.dist)
-    
 class MyGen(BaseGene):
-    maxInt = 100
-    mutProb = 0.05
+    mutProb = 0.01
     def mutate(self):
         self.value = self.randomValue()
-        #print "mutate"
     def randomValue(self):
-        r = MyRandom.randint(1,self.maxInt)
-        #print r
+        r = Instruction()
+        # r = 1
+        # print r
         return r
-        # print "randomValue"
-    def __add__(self, other):
-        print "add"
-        return (self.value + other.value) / 2
-    
+        
 genomeProto = {}
 for i in range(organismLen):
     genomeProto[str(i)] = MyGen
@@ -75,20 +26,11 @@ class MySpecimen(Organism):
     mutateOneOnly = False
     crossoverRate = 0.5
     def fitness(self):
-        fit = 0
-        for i in xrange(organismLen):
-            fit += self[str(i)]
-        return fit
+        return sha1(self.hashIt()).hexdigest()
     def mate(self, partner):
-        """
-        Mates this organism with another organism to
-        produce two entirely new organisms via random choice
-        of genes from this or the partner
-        """
         child1 = MySpecimen()
         child2 = MySpecimen()
     
-        # gene by gene, we assign our and partner's genes randomly
         for i in xrange(organismLen):
             if random() < self.crossoverRate:
                 child1.genes[str(i)].value = self[str(i)]
@@ -97,12 +39,16 @@ class MySpecimen(Organism):
                 child1.genes[str(i)].value = partner[str(i)]
                 child2.genes[str(i)].value = self[str(i)]
         
-        # done
         return (child1, child2)
+    def hashIt(self):
+        s = ""
+        for i in xrange(organismLen):
+            s += str(self[str(i)])
+        return s
     def __repr__(self):
         s = ""
         for i in xrange(organismLen):
-            s += " " + str(self[str(i)])
+            s += " " + str(self[str(i)]) + "\n"
         return "{" + s + " }[ " + str(self.fitness()) + " ]"
 
 class MyPopulation(Population):
@@ -134,17 +80,11 @@ class MyPopulation(Population):
 pop = MyPopulation()
 
 def main():
-    i = Instruction()
-    print i
-    return
     try:
-        print pop
         while True:
             pop.gen()
-            #print "GenSize: ", len(pop)
             best = pop.best()
             print best
-            #return
 
     except KeyboardInterrupt:
         pass
